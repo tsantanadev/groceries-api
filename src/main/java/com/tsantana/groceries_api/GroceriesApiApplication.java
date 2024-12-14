d3882aad0e175d09c291e788e61f09d558a14d7a
tsantanadev <thiago91098@gmail.com>
2024-12-14 15:48:12 -0300

    Foundation created

---
+++ src/main/java/com/tsantana/groceries_api/GroceriesApiApplication.java
@@ -0,0 +1,13 @@
+package com.tsantana.groceries_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceriesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsantana/groceries_api/GroceriesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsantana.groceries_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
